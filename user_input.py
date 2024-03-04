235878e272046f4c4e2a81c06973cdea81aaa254
Sharon404 <80156851+Sharon404@users.noreply.github.com>
2024-03-04 17:13:05 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = str(input("What is your name? "))
 age = int(input("How old are you "))
 locate = str(input("Where are you? "))
 
-print(f"hello {name} you are {age} years old and livje in {locate}")
+print(f"hello {name} you are {age} years old and live in {locate}")
