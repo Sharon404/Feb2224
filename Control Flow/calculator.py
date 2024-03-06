19d849e5b369c3c76fc9c3caff03a252353602f5
Sharon404 <aokosharo@gmail.com>
2024-03-06 22:42:57 +0300

    simple calculator

---
+++ Control Flow/calculator.py
@@ -0,0 +1,13 @@
+first = int(input("Enter the first number: "))
+second = int(input('Enter the second number: '))
+
+a = first + second
+b = first - second
+c = first * second
+d = first / second
+
+print(f'The sum of {first} and {second} is {a}')
+print(f'The difference between {first} and {second} is {b}')
+print(f'The product of {first} and {second} is {c}')
+print(f'The quotient of {first} and {second} is {d}')
+
