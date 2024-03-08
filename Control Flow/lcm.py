b7083d9b79b82f1cec090607ce02573f224e4d3f
Sharon404 <aokosharo@gmail.com>
2024-03-08 14:48:50 +0300

    lcm

---
+++ Control Flow/lcm.py
@@ -0,0 +1,19 @@
+def lcm(x, y):
+    if x > y:
+        z = x
+    else:
+        z = y
+        
+    while True:
+        #check if z is divisible by both x and y
+        if z % x == 0 and z % y == 0:
+            lcm = z
+            break
+        z +=1
+        
+    return lcm
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(lcm(num1, num2))
