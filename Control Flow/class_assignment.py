519bd140532130ecafd21e02ee532593ee5c91e5
Sharon404 <aokosharo@gmail.com>
2024-03-23 17:34:19 +0300

    classes and objects

--- Control Flow/class_assignment.py
+++ Control Flow/class_assignment.py
@@ -1,11 +1,13 @@
 class Person:
   def __init__(self, name, age, gender):
+      #creating class instance
     self.name = name
     self.age = age
     self.gender = gender
 
 p1 = Person("John", 36, "Male")
 
+#displaying the information of the person
 print(p1.name)
 print(p1.age)
 print(p1.gender)
